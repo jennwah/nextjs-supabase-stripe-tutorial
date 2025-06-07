907be3ca1ba8201d67335baa68581cde6f69f7ed
Lee Jenn Wah <olovepro@gmail.com>
2025-06-07 22:52:40 +0800

    commit shit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import NavItems from "./navitems";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -13,7 +20,14 @@ const Navbar = () => {
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign in</p>
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
